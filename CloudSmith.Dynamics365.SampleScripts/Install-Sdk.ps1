1d7d99c6223ec0dff815bc335739cadbeed7b919
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-09-17 20:05:04 -0400

    kickstart for updating the scripts

--- CloudSmith.Dynamics365.SampleScripts/Install-Sdk.ps1
+++ CloudSmith.Dynamics365.SampleScripts/Install-Sdk.ps1
@@ -1,52 +1,97 @@
-﻿#Greg
+﻿<# PSScriptInfo
+.VERSION 1.0.0
+.GUID e009159d-97e2-492a-a289-42426518dd41
+.AUTHOR CloudSmith Consulting LLC
+.COMPANYNAME CloudSmith Consulting LLC
+.COPYRIGHT (c) 2019 CloudSmith Consulting LLC.  All Rights Reserved.
+.TAGS Windows PowerShell Setup Dynamics CRM OneBox
+.LICENSEURI https://github.com/cloudsmithconsulting/Dynamics-ARM-Template/blob/master/LICENSE
+.PROJECTURI https://github.com/cloudsmithconsulting/Dynamics-ARM-Template
+.ICONURI 
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS 
+.EXTERNALSCRIPTDEPENDENCIES 
+.RELEASENOTES
+#>
+
+<# 
+.DESCRIPTION 
+ Installs the SDK
+#> 
+
+<#
+.SYNOPSIS 
+    Let's you quickly install the SDK on your computer
+
+.DESCRIPTION
+    This script was created to quickly and easily install the SDK on a computer
+
+.EXAMPLE
+    .\Install-Sdk -Path ".\Path\To\Install"
+    
+.Notes
+#>
+Param
+(
+	[string] $Path = ".\Tools"
+)
 
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $targetNugetExe = ".\nuget.exe"
-Remove-Item .\Tools -Force -Recurse -ErrorAction Ignore
+Remove-Item $Path -Force -Recurse -ErrorAction Ignore
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 
 ##
 ##Download Plugin Registration Tool
 ##
-./nuget install Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool -O .\Tools
-md .\Tools\PluginRegistration
-$prtFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool.'}
-move .\Tools\$prtFolder\tools\*.* .\Tools\PluginRegistration
-Remove-Item .\Tools\$prtFolder -Force -Recurse
+./nuget install Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool -O $Path
+$prtDestPath = Join-Path -Path $Path -ChildPath "PluginRegistration"
+md $prtDestPath
+$prtToolsFolder = Get-ChildItem $Path | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool.'}
+$prtToolsGlob = "$Path\$prtToolsFolder\tools\*.*"
+move $prtToolsGlob $prtDestPath
+$prtRootFolder = "$Path\$prtToolsFolder"
+Remove-Item $prtRootFolder -Force -Recurse
 
 ##
 ##Download CoreTools
 ##
-./nuget install  Microsoft.CrmSdk.CoreTools -O .\Tools
-md .\Tools\CoreTools
-$coreToolsFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.CoreTools.'}
-move .\Tools\$coreToolsFolder\content\bin\coretools\*.* .\Tools\CoreTools
-Remove-Item .\Tools\$coreToolsFolder -Force -Recurse
+./nuget install  Microsoft.CrmSdk.CoreTools -O $Path
+$coreDestPath = Join-Path -Path $Path -ChildPath "CoreTools"
+md $coreDestPath
+$coreToolsFolder = Get-ChildItem $Path | Where-Object {$_.Name -match 'Microsoft.CrmSdk.CoreTools.'}
+$coreToolsGlob = "$Path\$coreToolsFolder\content\bin\coretools\*.*"
+move $coreToolsGlob .\Tools\CoreTools
+$coreToolsRootFolder = "$Path\$coreToolsFolder"
+Remove-Item $coreToolsRootFolder -Force -Recurse
 
 ##
 ##Download Configuration Migration
 ##
-./nuget install  Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf -O .\Tools
-md .\Tools\ConfigurationMigration
-$configMigFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf.'}
+./nuget install  Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf -O $Path
+$ChildPath = Join-Path -Path $Path -ChildPath "ConfigurationMigration"
+md $ChildPath
+$configMigFolder = Get-ChildItem $Path | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf.'}
 move .\Tools\$configMigFolder\tools\*.* .\Tools\ConfigurationMigration
 Remove-Item .\Tools\$configMigFolder -Force -Recurse
 
 ##
 ##Download Package Deployer 
 ##
-./nuget install  Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O .\Tools
+./nuget install  Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O $Path
+$ChildPath = Join-Path -Path $Path -ChildPath "PackageDeployment"
+md $ChildPath
 md .\Tools\PackageDeployment
-$pdFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
+$pdFolder = Get-ChildItem $Path | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
 move .\Tools\$pdFolder\tools\*.* .\Tools\PackageDeployment
 Remove-Item .\Tools\$pdFolder -Force -Recurse
 
 ##
 ##Download Package Deployer PowerShell module
 ##
-./nuget install Microsoft.CrmSdk.XrmTooling.PackageDeployment.PowerShell -O .\Tools
-$pdPoshFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.PowerShell.'}
+./nuget install Microsoft.CrmSdk.XrmTooling.PackageDeployment.PowerShell -O $Path
+$pdPoshFolder = Get-ChildItem $Path | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.PowerShell.'}
 move .\Tools\$pdPoshFolder\tools\*.* .\Tools\PackageDeployment.PowerShell
 Remove-Item .\Tools\$pdPoshFolder -Force -Recurse
 
